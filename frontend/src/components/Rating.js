08c30d27d53e29b45b5c581f04c040698f33fbd2
DippreyB <48030605+DippreyB@users.noreply.github.com>
2021-07-06 22:30:01 -0500

    Backend auth, frontend login/regsiter

--- frontend/src/components/Rating.js
+++ frontend/src/components/Rating.js
@@ -3,7 +3,6 @@ import PropTypes from 'prop-types'
 import { propTypes } from 'react-bootstrap/esm/Image';
 
 const Rating = ({value, text, color}) => {
-
     let stars = [];
     for( let i  = 1; i < 6; i++){
         stars.push(<i key={`${new Date()}-star-${i}`} style={{color}}className={value >= i ? 'fas fa-star' : value >= i-0.5 ? 'fas fa-star-half-alt'  : 'far fa-star'} />)
@@ -22,7 +21,7 @@ Rating.defaultProps = {
     color: '#f8e825'
 }
 Rating.propTypes = {
-    value: PropTypes.number.isRequired,
+    value: PropTypes.number,
     text: PropTypes.string.isRequired,
     color: PropTypes.string,
 
